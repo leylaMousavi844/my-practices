9addc3160d81d60d763d677520771fc1dead7cab
leyla <leyla8443203@gmail.com>
2024-01-24 09:50:24 -0800

    first-commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// average:
+let a = 10;
+let b = 15
+function average(a,b){
+    return(a+b)/2
+}
+let result=average(a,b)
+console.log(result);
+
+
+
+// while-practice:
+let i = 0;
+while(i<5){
+    console.log(i*2)
+    i++
+}
