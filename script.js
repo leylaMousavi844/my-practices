094eb49e614a57a43a29e7b75463dfe1bec3f689
leyla <leyla8443203@gmail.com>
2024-01-24 10:44:59 -0800

    add-for-in-practice

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ while(i<5){
     console.log(i*2)
     i++
 }
+
+// for in practice:
+let names=['mohsen','leyla','shayan','danial'];
+for(let index in names){
+    console.log(index)
+    console.log(names[index])
+    console.log(names[2])
+}
+
+let user={
+    name:"maryam",
+    login:true,
+    id:24
+}
+for(let key in user){
+    console.log(key)
+    console.log(user[key])
+    console.log(user["login"])
+}
+
