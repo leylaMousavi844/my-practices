c49ea2c3dd62534958799478f60cba1a9062f58e
leyla <leyla8443203@gmail.com>
2024-02-21 04:24:27 -0800

    new-practices

--- script.js
+++ script.js
@@ -156,12 +156,13 @@
 
 
 
+
 // how to copy of refreence typs:
-let car={
-    brand:"saypa",
-    year:2019,
-    color:"black",  
-}
+// let car={
+//     brand:"saypa",
+//     year:2019,
+//     color:"black",  
+// }
 
 // for(let key in car){
 //     car[Key]=car2[key];
@@ -176,6 +177,8 @@ let car={
 // console.log(car2)
 
 
+
+
 // Math:
 // choose the one number between two number:
 // function genreatRand (min,max){
@@ -185,3 +188,331 @@ let car={
 
 // let num =1.6;
 // console.log(Math.round(num));
+
+
+
+// Date:
+// let d1= new Date();
+// console.log(d1);
+// let d2 =new Date("2023-02-05");
+// console.log(d2);
+// console.log(d2.getFullYear());
+// console.log(d2.getSeconds());
+// console.log(d2.toISOString());
+
+
+
+// add data to arrays:
+// let arr=[1,2,3,4,5];
+// arr.push(6);
+// console.log(arr);
+// arr.unshift(10);
+// console.log(arr);
+// arr.splice(1,2,5);
+// console.log(arr);
+// console.log( arr.splice(1,2,5))
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.splice(1,2);
+// console.log(arr);
+
+
+
+// find index in arrays:
+// let arr=[1,2,3,4,5,5,1,5,2];
+// console.log(arr.indexOf(5));
+// console.log(arr.lastIndexOf(2));
+// console.log(arr.includes(6));
+
+// let student=[
+//     {grad:"A",age:20},
+//     {grad:"B",age:23},
+//     {grad:"c",age:19}
+// ]
+// console.log(student.find((el)=>el.age===23));
+// console.log(student.find((el)=>el.age===26));
+
+
+
+// arrays:
+// let numbers=[10,5,25,63,45,78,41];
+// let anyNumbers=[14,22,78,20,12];
+// let newArray= numbers.concat(anyNumbers);
+// console.log(newArray);
+// console.log(numbers.slice(1,5));
+// let anyNumbers2=anyNumbers.slice(1);
+// console.log(anyNumbers2);
+// let newArray=[1,...numbers,12,...anyNumbers];
+// console.log(newArray);
+// let arr1=[...numbers];
+// console.log(arr1);
+
+
+// foreach:
+// let products = [
+//     { title: "mobile", price: "1000" },
+//     { title: "tablet", price: "1500" },
+//     { title: "labtop", price: "2000" },
+//     { title: "pc", price: "3000" }
+// ]
+// let max={title:"",price:0}
+
+// products.forEach(function (item) {
+//     if(item.price>max.price){
+//         max.price=item.price;
+//         max.title=item.title
+
+//     }
+// })
+// console.log(max);
+
+
+
+
+// convert array to string:
+// let arr =[0,1,1,2,5,6];
+// console.log(arr.join("-"))
+// convert string to array with split() 
+
+
+
+
+// every & some:
+// let arr=[10,15,8,20,12,17];
+// let result=arr.every(function(value){
+//     return value>=10
+// })
+// let result=arr.some(function(value){
+//     return value< 10
+// })
+// console.log(result);
+
+
+
+
+// filter:
+// let arr=[15,20,19,14,12,16];
+// let result=arr.filter(function(value){
+//     return value>=15;
+// })
+// console.log(result);
+
+
+
+// map:
+// let arr=['sara','reza','mona','roya','mohammad'];
+// let result=arr.map(value=>`<span>${value}</span>`);
+// console.log(result);
+// let result=arr.map(value=>({name:value}));
+// console.log(result);
+
+
+
+
+
+
+
+// practice1:
+// let student = [
+//     {
+//         name: "Ali",
+//         age: 20,
+//         grad: 90
+//     },
+//     {
+//         name: "Mohammad",
+//         age: 18,
+//         grad: 40
+//     },
+//     {
+//         name: "Reza",
+//         age: 17,
+//         grad: 80
+//     }
+// ]
+// let max = { maxName: "", grad: 0 }
+// student.forEach(function (item) {
+//     if (item.grad > max.grad) {
+//         max.grad = item.grad;
+//         max.maxName = item.name
+//     }
+// })
+// console.log(max);
+
+
+// practice 2:
+// let arr = [4,2,7,1];
+// let sortedArr = [];
+
+// const arrLength = arr.length;
+
+// for(let i=0; i < arrLength; i++) {
+//     let min = arr[0];
+//     for(let j=1; j < arr.length; j++) {
+
+//         if(arr[j] < min) {   
+//             min = arr[j];
+//         }
+
+//     }
+
+//     sortedArr.push(min);
+//     arr.splice(arr.findIndex((item) => item == min), 1)
+//     if(arr.length === 1) {
+//         sortedArr.push(arr[0])
+//         break;
+//     }
+// }
+
+// console.log(sortedArr)
+// چیزی که دراین تمرین یاد گرفتم این بود که
+//  اگر میخواهیم حلقه ی   داخلی که با متغیر جی ایجاد کرده ایم تا آخر عمل کند باید
+//  .حلقه ی بیرونی تا آخرین عضو بچرخد.به همین دلیل آی را یک ثابت در نظر گرفتیم
+// ودوما دلیل اینکه ما از دوحلقه ی تودر تو استفاده میکنیم این است که یکی را ثابت نگه دارد 
+// وبقیه را با آن مقایسه کند.
+
+
+
+// practice3:
+// let products = [
+//     {id: "1", title: "phone", price: "9000"},
+//     {id: "10", title: "pc", price: "21000"},
+//     {id: "14", title: "tablet", price: "12000"},
+//     {id: "54", title: "power", price: "19000"},
+//     {id: "67", title: "watch", price: "17000"}
+// ];
+
+// let userSelection = [1,54,67];
+
+// let userCart = [];
+
+// products.forEach((item, index) => {
+//     let res = userSelection.findIndex((userSelectionItem) => userSelectionItem === parseInt(item.id))
+
+//     res !== -1? userCart.push(item) : null;
+
+// })
+
+
+// // sorted user cart
+// let sortedUserCart = [];
+
+
+// userCart.forEach((item, index) => {
+//     let min = 0;
+//     userCart.forEach((otherItem, otherItemIndex) => {
+//         if(parseInt(userCart[min].price) > parseInt(otherItem.price)) {
+//             min = otherItemIndex;
+//         }
+//     })
+//     sortedUserCart.push(userCart[min]);
+//     userCart.splice(min, 1);
+//     if(userCart.length === 1) {
+//         sortedUserCart.push(userCart[0]);
+//     }
+// });
+
+
+// console.log(sortedUserCart);
+// دراین تمرین یاد گرفتم که تابع 
+//   چگونه عمل میکند findIndex  
+// این تابع اگر اندیس عضو مورد نظر راپیا کرد 
+// که آنرا برمیگرداند واگر پیدا نکرد عدد منفی یک را برمیگرداند
+
+
+
+
+
+// var students = [
+//     {id: 1, name: "Ali", age: 20},
+//     {id: 2, name: "Alireza", age: 23},
+//     {id: 13, name: "Maryam", age: 26},
+//     {id: 14, name: "Mohammadali", age: 19},
+//     {id: 5, name: "Mohammad", age: 22},
+//     {id: 9, name: "Fateme", age: 21}
+// ]
+// let newArr=[]
+// let searchQuery=window.prompt("inter your name");
+// console.log(searchQuery);
+// students.forEach((item)=>{
+//     if(item.name.toLowerCase().includes(searchQuery.toLowerCase())){
+// newArr.push(item)
+//     }
+// })
+// console.log(newArr)
+
+
+
+// practice4:
+// let person = [
+//     {gender:"mard", name: "Hossein", age: 23},
+//     {gender:"mard", name: "Ariyan", age: 22},
+//     {gender:"mard", name: "Alireza", age: 21},
+//     {gender:"mard", name: "Reza", age: 20},
+//     {gender:"mard", name: "Amir", age: 19},
+//     {gender:"zan", name: "Sahar", age: 18},
+//     {gender:"zan", name: "Maryam", age: 17},
+//     {gender:"zan", name: "Negin", age: 16},
+//     {gender:"zan", name: "Melorin", age: 15},
+//     {gender:"zan", name: "Deniz", age: 14},    
+// ];
+// let results = [];
+
+// let searchQuery = prompt("Enter Gender");
+// let searchQuery2 = prompt("Enter Age");
+
+// person.forEach((item) => {
+//     if(item.gender.toLowerCase().includes(searchQuery.toLowerCase()) && item.age >= parseInt(searchQuery2)){
+//         results.push(item)
+//     }
+// });
+// console.log(results);
+// //__________________________________________________
+// // Show Name with an alert :
+// //__________________________________________________
+// let names = "";
+// if(results.length) {
+//     results.forEach((item) => {
+//         names = item.name + ',' + names;
+//     });
+// } else {
+//     names="nothing found!";
+// }
+// alert(names);
+
+
+
+
+
+// practice5:
+var products = [
+    { id: 1, name: "laptop", price: 40000000 },
+    { id: 2, name: "phone", price: 10000000 },
+    { id: 13, name: "pen", price: 10000 },
+    { id: 4, name: "cookie", price: 35000 },
+    { id: 5, name: "milk", price: 47000 },
+    { id: 9, name: "pencil", price: 7000 }
+];
+
+var counter = {
+    4: { available: true },
+    5: { available: false },
+    9: { available: true },
+    1: { available: false },
+    2: { available: false },
+    13: { available: true },
+};
+
+let newProducts=[]
+for (let index = 0; index < products.length; index++) {
+    for (let key in counter) {
+       if(products[index].id==key){
+        newProducts.push(products[index],counter[key])
+  
+       }
+    }
+
+}
+console.log(newProducts)
